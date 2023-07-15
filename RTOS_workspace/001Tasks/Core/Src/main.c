2fd9a405896da1a1c585f7f8a29628431008d402
RafalMielewczyk <rafal.mielewczyk4@gmail.com>
2023-07-15 18:38:03 +0200

    After lecture 36

--- RTOS_workspace/001Tasks/Core/Src/main.c
+++ RTOS_workspace/001Tasks/Core/Src/main.c
@@ -45,6 +45,8 @@
 
 /* USER CODE BEGIN PV */
 
+#define DWT_CTRL	(*(volatile uint32_t*)0xE0001000)
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -96,6 +98,13 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
+
+  // Enable the CYCCNT counter
+  DWT_CTRL |= (1 << 0);
+
+  SEGGER_SYSVIEW_Conf();
+
+  SEGGER_SYSVIEW_Start();
 
   status = xTaskCreate(task1_handler, "Task-1", 200, "Hello world from Task-1", 2, &task1_handle);
 
@@ -316,7 +325,7 @@ static void task1_handler(void* parameters)
 	while(1)
 	{
 		printf("%s\n", (char*)parameters);
-		taskYIELD();
+//		taskYIELD();
 	}
 }
 
@@ -325,7 +334,7 @@ static void task2_handler(void* parameters)
 	while(1)
 	{
 		printf("%s\n", (char*)parameters);
-		taskYIELD();
+//		taskYIELD();
 	}
 }
 /* USER CODE END 4 */
